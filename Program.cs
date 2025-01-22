7836c7ba756b0ab249a6bb61ae1437c26ddedf78
Rostyslav Mosiichuk <vtk241_mri@student.ztu.edu.ua>
2025-01-22 15:00:39 +0200

    Create project

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть перше число:");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть друге число:");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = number1 + number2;
+
+        Console.WriteLine($"Сума чисел {number1} і {number2} дорівнює {sum}");
+    }
+}
